4d60e211b593544b141a841c260132bce6fe2852
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-09-17 15:46:45 +0600

    Hisam-test-3

--- bright-space-client/src/App.jsx
+++ bright-space-client/src/App.jsx
@@ -4,13 +4,9 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <h1>Edited By Hisam</h1>
-=======
       <h2 className="text-3xl">Staging 2</h2>
       <h2 className="text-3xl">Staging 2</h2>
       <h2 className="text-3xl">Staging 2</h2>
->>>>>>> e1fc972b92205817914d643fe91e7e2067edb877
     </>
   )
 }

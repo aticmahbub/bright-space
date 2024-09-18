b894bd1cea6ce9fd461df9c57481193e850fa2b6
aticcmahbub <aticcmahbub@gmail.com>
2024-09-18 16:10:06 +0600

    basic routes setup

--- bright-space-client/src/Routes/Routes.jsx
+++ bright-space-client/src/Routes/Routes.jsx
@@ -2,8 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/shared/Authentication/Login/Login";
-import Login from "../pages/shared/Authentication/Registration/Registration";
+import Registration from "../pages/shared/Authentication/Registration/Registration";
 import Dashboard from "../Layouts/Dashboard";
+import About from "../pages/About/About";
+import Features from "../pages/Features/Features";
+import Contact from "../pages/Contact/Contact";
+import StudentProfile from "../pages/StudentProfile/StudentProfile";
+import Blog from "../pages/Blog/Blog";
+import Forum from "../pages/Forum/Forum";
+import UserProfile from "../pages/UserProfile/UserProfile";
+import TeacherProfile from "../pages/TeacherProfile/TeacherProfile";
 
 
 export const router = createBrowserRouter([
@@ -23,6 +31,30 @@ export const router = createBrowserRouter([
                 path: '/registration',
                 element: <Registration/>
             },
+            {
+                path: '/about',
+                element: <About/>
+            },
+            {
+                path: '/features',
+                element: <Features/>
+            },
+            {
+                path: '/contact',
+                element: <Contact/>
+            },
+            {
+                path: '/blog',
+                element: <Blog/>
+            },
+            {
+                path: '/forum',
+                element: <Forum/>
+            },
+            {
+                path: '/userProfile',
+                element: <UserProfile/>
+            },
         ]
     },
     {
@@ -31,7 +63,14 @@ export const router = createBrowserRouter([
         children:[
             // student routes
             {
-                
+                path: 'studentProfile',
+                element: <StudentProfile/>
+            },
+
+            // teacher routes
+            {
+                path: 'teacherProfile',
+                element: <TeacherProfile/>
             }
         ]
     }

0a0888fd5ed26ded594ffb93de75baf6058b2add
saidulislam28 <saidulislam79028@gmail.com>
2024-09-18 18:47:20 +0600

    complete Our Team section with mobile responsiveness

--- bright-space-client/src/pages/About/About/About.jsx
+++ bright-space-client/src/pages/About/About/About.jsx
@@ -3,11 +3,13 @@
 */
 
 import Faq from "../Faq/Faq";
+import Team from "../Team/Team";
 
 const About = () => {
     return (
         <div>
             <Faq />
+            <Team></Team>
         </div>
     );
 };

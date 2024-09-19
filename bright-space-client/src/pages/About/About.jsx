a1acb3e1f4faebd950b1a3ed5104532302b0b36a
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-09-19 20:41:36 +0600

    Design About Page Create Contact section , Mission Section and added this about page. Upload some images in assests/About folder. This folder carry only about page images

--- bright-space-client/src/pages/About/About.jsx
+++ bright-space-client/src/pages/About/About.jsx
@@ -1,7 +1,17 @@
+import AboutBanner from "./AboutBanner/AboutBanner";
+import Contact from "./contact/Contact";
+import Faq from "./Faq/Faq";
+import MIssion from "./Mission/MIssion";
+import Team from "./Team/Team";
+
 const About = () => {
     return (
         <div>
-            About
+            <AboutBanner />
+            <MIssion />
+            <Faq />
+            <Team />
+            <Contact />
         </div>
     );
 };

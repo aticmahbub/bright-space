1ffae9422c505b30d3072b43b2aca0bebc3e2be6
Abu Naim <abunaim140@gmail.com>
2024-09-20 00:27:07 +0600

    added classes page, class card, classes json file  and added some changes in navbar, banner, footer, Main, routes, main files

--- bright-space-client/src/pages/Home/Banner/Banner.jsx
+++ bright-space-client/src/pages/Home/Banner/Banner.jsx
@@ -3,16 +3,16 @@ import './css/banner.css'
 
 const Banner = () => {
     return (
-        <div className="my-5 bg-[#F7F8FD]">
-            <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row justify-between items-center gap-5 py-20">
+        <div className='h-[calc(100vh-102px)]'>
+            <div className="h-full flex flex-col-reverse md:flex-row justify-between items-center gap-5 py-6">
                 {/* text section */}
-                <div className='py-12 md:py-28 space-y-5 max-w-full md:max-w-[40%] p-3'>
+                <div className='space-y-5 flex-1'>
                     <h5 className="text-pink-400 font-bold">EDUCATION SOLUTION</h5>
                     <h2 className='text-4xl md:text-5xl font-bold'>Empower Your Learning with Virtual Classrooms.</h2>
                     <h3 className='font-semibold text-xl md:text-2xl'>
                         An interactive platform for teachers to share resources, assignments, and grades with students, making learning easy and accessible.
                     </h3>
-                    <div className='flex gap-5 md:gap-10 '>
+                    <div className='flex gap-5 md:gap-8 '>
                         <button className='border px-6 py-3 md:px-8 md:py-4 font-semibold bg-[#5F2EED] text-white hover:bg-white hover:text-black hover:border-[#5F2EED]'>
                             View Courses
                         </button>
@@ -23,8 +23,8 @@ const Banner = () => {
                 </div>
 
                 {/* image section */}
-                <div className=''>
-                    <img className='h-[350px] md:h-[550px] hover:animate-shake' src={bannerImg} alt="Virtual Classroom" />
+                <div className='flex-1'>
+                    <img className='hover:animate-shake w-full' src={bannerImg} alt="Virtual Classroom" />
                 </div>
             </div>
         </div>

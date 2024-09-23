dadfd33a30c64606e627fe91ada9a675f5e84445
Abu Naim <abunaim140@gmail.com>
2024-09-23 21:30:41 +0600

    added join class modal component

---
+++ bright-space-client/src/components/JoinClsModal/JoinClsModal.jsx
@@ -0,0 +1,52 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Box,
+    Text,
+    Avatar,
+    NumberInput,
+    NumberInputField,
+} from '@chakra-ui/react'
+
+const ModalPrimary = ({ title, isOpen, onClose }) => {
+    return (
+        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+            <ModalOverlay />
+            <ModalContent rounded='none'>
+                <ModalHeader>{title}</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Box border='1px' borderColor='gray' p='4'>
+                        <Text>You&apos;re currently signed in as</Text>
+                        <Box display='flex' gap='3' mt='3'>
+                            <Avatar size='md' name='Ryan Florence' src='https://bit.ly/ryan-florence' />
+                            <Box>
+                                <Text fontWeight='600'>John Doe</Text>
+                                <Text>johndoe@mail.com</Text>
+                            </Box>
+                        </Box>
+                    </Box>
+                    <Box border='1px' borderColor='gray' p='4' mt='4'>
+                        <Text>Class code</Text>
+                        <Text>Ask your teacher for the class code, then enter it here.</Text>
+                        <NumberInput size='lg' maxW={52} min={7} mt='4'>
+                            <NumberInputField placeholder='Class code' rounded='none' fontSize='medium' />
+                        </NumberInput>
+                    </Box>
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick={onClose} colorScheme='primary' rounded='none' variant='ghost'>Close</Button>
+                    <Button colorScheme='secondary' rounded='none' variant='ghost'>Join</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    );
+};
+
+export default ModalPrimary;
--- bright-space-client/src/pages/ClassRoom/ClassRoom.jsx
+++ bright-space-client/src/pages/ClassRoom/ClassRoom.jsx
@@ -1,16 +1,20 @@
 import classroom from '../../Lotties/classroom.json'
-import { Box, Button, Text } from "@chakra-ui/react";
+import { Box, Button, Text, useDisclosure } from "@chakra-ui/react";
 import Lottie from "lottie-react";
+import ModalPrimary from '../../components/JoinClsModal/JoinClsModal';
 
 const ClassRoom = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
         <Box className='h-[calc(100vh-101px)]'>
+            <ModalPrimary title='Join Class' isOpen={isOpen} onClose={onClose} />
             <Box h='full' display='flex' gap='8' alignItems='center' justifyContent='center' flexDir='column'>
-                <Lottie className='w-96' animationData={classroom} />
+                <Lottie className='w-full md:w-96' animationData={classroom} />
                 <Box>
                     <Text textAlign='center' mb='4'>Add a class to get started</Text>
                     <Button colorScheme='secondary' rounded='none' variant='outline'>Create Class</Button>
-                    <Button colorScheme='secondary' rounded='none' ml='6'>Join Class</Button>
+                    <Button onClick={onOpen} colorScheme='secondary' rounded='none' ml='6'>Join Class</Button>
                 </Box>
             </Box>
         </Box>

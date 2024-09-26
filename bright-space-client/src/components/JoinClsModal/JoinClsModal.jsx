3eaa68cc7f638f58eaec8d7bfa23131efc193020
Abu Naim <abunaim140@gmail.com>
2024-09-26 22:20:16 +0600

    adjust styles in classroom page component

--- bright-space-client/src/components/JoinClsModal/JoinClsModal.jsx
+++ bright-space-client/src/components/JoinClsModal/JoinClsModal.jsx
@@ -18,11 +18,11 @@ const JoinClsModal = ({ title, isOpen, onClose }) => {
     return (
         <Modal onClose={onClose} isOpen={isOpen} isCentered>
             <ModalOverlay />
-            <ModalContent rounded='none'>
+            <ModalContent rounded='base'>
                 <ModalHeader>{title}</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
-                    <Box border='1px' borderColor='gray' p='4'>
+                    <Box border='1px' borderColor='gray' rounded='base' p='4'>
                         <Text>You&apos;re currently signed in as</Text>
                         <Box display='flex' gap='3' mt='3'>
                             <Avatar size='md' name='Ryan Florence' src='https://bit.ly/ryan-florence' />
@@ -32,17 +32,17 @@ const JoinClsModal = ({ title, isOpen, onClose }) => {
                             </Box>
                         </Box>
                     </Box>
-                    <Box border='1px' borderColor='gray' p='4' mt='4'>
+                    <Box border='1px' borderColor='gray' rounded='base' p='4' mt='4'>
                         <Text>Class code</Text>
                         <Text>Ask your teacher for the class code, then enter it here.</Text>
                         <NumberInput size='lg' maxW={52} min={7} mt='4'>
-                            <NumberInputField placeholder='Class code' rounded='none' fontSize='medium' />
+                            <NumberInputField placeholder='Class code' rounded='base' fontSize='medium' />
                         </NumberInput>
                     </Box>
                 </ModalBody>
-                <ModalFooter>
-                    <Button onClick={onClose} colorScheme='primary' rounded='none' variant='ghost'>Close</Button>
-                    <Button colorScheme='secondary' rounded='none' variant='ghost'>Join</Button>
+                <ModalFooter gap='4'>
+                    <Button onClick={onClose} colorScheme='primary' rounded='base' variant='ghost'>Close</Button>
+                    <Button colorScheme='primary' rounded='base'>Join</Button>
                 </ModalFooter>
             </ModalContent>
         </Modal>

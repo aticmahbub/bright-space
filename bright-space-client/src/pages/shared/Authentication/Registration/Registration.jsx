cf02a3811cc56933038241bc23588c49ad6da749
aticcmahbub <aticcmahbub@gmail.com>
2024-09-26 17:12:46 +0600

    updated user profile added

--- bright-space-client/src/pages/shared/Authentication/Registration/Registration.jsx
+++ bright-space-client/src/pages/shared/Authentication/Registration/Registration.jsx
@@ -4,11 +4,12 @@ import { AuthContext } from "../../../../providers/AuthProvider";
 
 const Registration = () => {
 
-  const {createUser} = useContext(AuthContext)
+  const {createUser, updateUserProfile} = useContext(AuthContext)
   const [formData, setFormData] = useState({
     fullName: "",
     email: "",
     password: "",
+    photoURL: "",
     termsAccepted: false,
   });
 
@@ -28,6 +29,13 @@ const Registration = () => {
     .then(result =>{
       const loggedUser = result.user
       console.log(loggedUser);
+      updateUserProfile(formData.fullName, formData.photoURL)
+      .then(()=>{
+        console.log('user profile updated');
+      })
+      .catch(error =>{
+        console.log(error);
+      })
     })
 
   };
@@ -63,6 +71,19 @@ const Registration = () => {
                   value={formData.fullName}
                   onChange={handleChange}
                   placeholder="Enter your Name"
+                  className="w-full px-4 py-3 border border-gray-100 rounded-lg bg-[#FCFCFD]  focus:outline-none focus:ring focus:ring-indigo-200"
+                />
+              </div>
+              <div className="text-start flex flex-col">
+                <label className="mb-2 font-medium text-gray-700">
+                  Photo URL
+                </label>
+                <input
+                  type="text"
+                  name="photoURL"
+                  value={formData.photoURL}
+                  onChange={handleChange}
+                  placeholder="Enter your photo url"
                   className="w-full px-4 py-3 border border-gray-100 rounded-lg bg-[#FCFCFD]  focus:outline-none focus:ring focus:ring-indigo-200"
                 />
               </div>

d8788eb946c18f46bd660ddc55087f8ca6974fae
aticcmahbub <aticcmahbub@gmail.com>
2024-09-26 13:30:05 +0600

    Merge branch 'staging' of https://github.com/aticmahbub/bright-space into staging

---
+++ bright-space-client/src/pages/Courses/AllCourses/AllCourses.jsx
@@ -0,0 +1,25 @@
+import { Box } from "@chakra-ui/react";
+import ClassCard from "../../../components/ClassCard/ClassCard";
+import { useEffect, useState } from "react";
+
+const Courses = () => {
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        fetch('classes.json')
+            .then(res => res.json())
+            .then(data => setClasses(data))
+    }, []);
+
+    return (
+        <Box display='flex' alignItems='center' py='20'>
+            <Box className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-7'>
+                {
+                    classes.map((cls, idx) => <ClassCard key={idx} cls={cls} />)
+                }
+            </Box>
+        </Box>
+    );
+};
+
+export default Courses;

b6a76af93d09f7fe707d2f7d9a95e0284e00865a
aticcmahbub <aticcmahbub@gmail.com>
2024-09-26 15:31:38 +0600

    add: registration implemented

--- bright-space-client/src/providers/AuthProvider.jsx
+++ bright-space-client/src/providers/AuthProvider.jsx
@@ -5,7 +5,6 @@ import { app } from "../Firebase/firebase.config";
 
 export const AuthContext =createContext(null)
 const auth = getAuth(app)
-
 const AuthProvider = ({children}) => {
     const [user, setUser] =useState(null)
     const [loading, setLoading] =useState(true)

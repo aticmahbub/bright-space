caffd4571e7dd337834bc53948563f257ee8bd2f
Abu Naim <abunaim140@gmail.com>
2024-09-28 20:05:42 +0600

    added website logo and adjust the ui

--- bright-space-client/src/pages/shared/Navbar/Navbar.jsx
+++ bright-space-client/src/pages/shared/Navbar/Navbar.jsx
@@ -1,9 +1,5 @@
-/**
- * Created By Hisam, 17/9/24
- */
-
-import { Avatar, Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, ButtonGroup, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, Heading, IconButton, Menu, MenuButton, MenuGroup, MenuItem, MenuList, Show, Spacer, useDisclosure } from "@chakra-ui/react";
-
+import { Avatar, Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, ButtonGroup, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, IconButton, Image, Menu, MenuButton, MenuGroup, MenuItem, MenuList, Show, Spacer, useDisclosure } from "@chakra-ui/react";
+import logo from '../../../assets/bright-space-logo.svg'
 import { Link, NavLink } from 'react-router-dom';
 // import useRole from '../../../hooks/useRole';
 import { useRef } from 'react';
@@ -18,21 +14,9 @@ import useEnrolls from "../../../hooks/useEnrolled";
 const Navbar = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const btnRef = useRef();
-
     const { user, logOut } = useAuth()
-    const [enrolls] =useEnrolls()
-    // const navLi = <>
-    //     <NavLink to="/" className='mainMenu-style'>Home</NavLink>
-    //     <NavLink to="/allCourses" className='mainMenu-style'>Courses</NavLink>
-    //     <NavLink to="/classroom" className='mainMenu-style'>Classroom</NavLink>
-    //     <NavLink to="/features" className='mainMenu-style'>Features</NavLink>
-    //     <NavLink to="/about" className='mainMenu-style'>About</NavLink>
-    //     <NavLink to="/contact" className='mainMenu-style'>Contact us</NavLink>
-    //     <NavLink to="/blog" className='mainMenu-style'>Blog</NavLink>
-    //     {<NavLink to="/addCourse" className='mainMenu-style'>AddCourse</NavLink>}
-    //     <NavLink to="/support" className='mainMenu-style'>Support</NavLink>
-        
-    // </>
+    const [enrolls] = useEnrolls()
+
     const navItems = [
         {
             name: 'Home',
@@ -79,7 +63,7 @@ const Navbar = () => {
             .catch(error => console.log(error))
     }
     return (
-        <Box px={{base: '2', lg: 8}} pt='5' bg='#F7F7F8'>
+        <Box px={{ base: '2', lg: 8 }} pt='5' bg='#F7F7F8'>
             <NavAdvertise />
             <Box px={{ base: 2, md: 7 }} py='4' w='full'>
                 <Flex maxW='1596px' mx='auto' alignItems='center' gap={{ base: 1, md: 2 }}>
@@ -115,30 +99,32 @@ const Navbar = () => {
                                 </DrawerContent>
                             </Drawer>
                         </Show>
-                        <Heading size={{ base: 'md', md: 'lg' }} as={Link} to='/' textColor='primary.500'>Bright<span className=''>Space</span></Heading>
-                    </Box>
-                    <Spacer />
-                    <Show above="lg">
-                        <Box>
-                            <Breadcrumb separator='' spacing='5'>
-                                {
-                                    navItems.map((item, idx) => <BreadcrumbItem key={idx}>
-                                        {
-                                            item.name !== 'Pages' ? <BreadcrumbLink as={NavLink} to={item.path} _hover={{ color: 'primary.500', textDecoration: 'none' }} _activeLink={{ color: 'primary.500', fontWeight: 'semibold' }}>{item.name}</BreadcrumbLink> : <Menu isOpen={isOpen} isLazy placement="bottom">
-                                                <MenuButton onMouseEnter={onOpen} transition='all 0.001s' _hover={{ color: 'primary.500' }}>{item.name}</MenuButton>
-                                                <MenuList onMouseLeave={onClose} mt='4' borderRadius='base'>
-                                                    {
-                                                        item.pages.map((page, idx) => <MenuItem key={idx} as={NavLink} _activeLink={{ color: 'primary.500' }} to={page.path}>{page.name}</MenuItem>)
-                                                    }
-                                                </MenuList>
-                                            </Menu>
-                                        }
+                        <Image w={{ base: 5, md: 7 }} src={logo} alt='bright space logo' />
+                        {/* <Heading size={{ base: 'md', md: 'lg' }} as={Link} to='/' textColor='primary.500'>Bright<span className=''>Space</span></Heading> */}
 
-                                    </BreadcrumbItem>)
-                                }
-                            </Breadcrumb>
-                        </Box>
-                    </Show>
+                        <Spacer />
+                        <Show above="lg">
+                            <Box>
+                                <Breadcrumb separator='' spacing='5'>
+                                    {
+                                        navItems.map((item, idx) => <BreadcrumbItem key={idx}>
+                                            {
+                                                item.name !== 'Pages' ? <BreadcrumbLink as={NavLink} to={item.path} _hover={{ color: 'primary.500', textDecoration: 'none' }} _activeLink={{ color: 'primary.500', fontWeight: 'semibold' }}>{item.name}</BreadcrumbLink> : <Menu isOpen={isOpen} isLazy placement="bottom">
+                                                    <MenuButton onMouseEnter={onOpen} transition='all 0.001s' _hover={{ color: 'primary.500' }}>{item.name}</MenuButton>
+                                                    <MenuList onMouseLeave={onClose} mt='4' borderRadius='base'>
+                                                        {
+                                                            item.pages.map((page, idx) => <MenuItem key={idx} as={NavLink} _activeLink={{ color: 'primary.500' }} to={page.path}>{page.name}</MenuItem>)
+                                                        }
+                                                    </MenuList>
+                                                </Menu>
+                                            }
+
+                                        </BreadcrumbItem>)
+                                    }
+                                </Breadcrumb>
+                            </Box>
+                        </Show>
+                    </Box>
                     <Spacer />
                     <Box display='flex' alignItems='center'>
                         {

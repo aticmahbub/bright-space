a17b2f6922f34228182e81233733876792334449
Abu Naim <abunaim140@gmail.com>
2024-09-29 23:05:06 +0600

    added responsive styles and swiper in partners section

--- bright-space-client/src/pages/Home/Partners/Partners.jsx
+++ bright-space-client/src/pages/Home/Partners/Partners.jsx
@@ -6,18 +6,55 @@ import amazon from '../../../assets/logos/amazon.svg';
 import adobe from '../../../assets/logos/adobe.svg';
 import notion from '../../../assets/logos/notion.svg';
 import netflix from '../../../assets/logos/netflix.svg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+// import { Pagination } from 'swiper/modules';
+
 
 const partnersLogo = [zapier, spotify, zoom, amazon, adobe, notion, netflix];
 
 const Partners = () => {
     return (
         <Box maxW='1596px' mx='auto' px={{ base: '2', lg: 8, '2xl': 0 }}>
-            <Box display='flex' alignItems='center' justifyContent='space-between' p='8' border='1px' borderColor='#F1F1F3' rounded='base' bg='#FCFCFD'>
-                {
-                    partnersLogo.map((logo, idx) => <Box key={idx} display='flex' justifyContent='center' w='full' px='10' py='7' borderRight={idx !== 6 ? '1px': 'none'} borderColor='#F1F1F3'>
-                        <Image src={logo} alt='partner logo' />
-                    </Box>)
-                }
+            <Box p={{ base: 2, lg: 6, '2xl': 7 }} border='1px' borderColor='#F1F1F3' rounded='base' bg='#FCFCFD'>
+                <Swiper
+                    slidesPerView={1}
+                    // style={{
+                    //     paddingBottom: 45,
+                    //     '--swiper-pagination-color': '#FF9500'
+                    // }}
+                    pagination={{
+                        clickable: true,
+                        // dynamicBullets: true
+                    }}
+                    breakpoints={{
+                        320: {
+                            slidesPerView: 2,
+                        },
+                        768: {
+                            slidesPerView: 4,
+                        },
+                        992: {
+                            slidesPerView: 5,
+                        },
+                        1080: {
+                            slidesPerView: 7,
+                        },
+                    }}
+                    // modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    {
+                        partnersLogo.map((logo, idx) => <SwiperSlide key={idx}><Box display='flex' justifyContent='center' w='full' px={{ base: 7, '2xl': 10 }} py={{ base: 5, '2xl': 7 }} borderRight={idx !== 6 ? '1px' : 'none'} borderColor='#F1F1F3'>
+                            <Image src={logo} alt='partner logo' />
+                        </Box></SwiperSlide>)
+                    }
+                </Swiper>
             </Box>
         </Box>
     );

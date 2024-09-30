90fc77a7d9b07ca870a32d0347b63741a95f4387
Abu Naim <abunaim140@gmail.com>
2024-09-30 20:49:40 +0600

    added view all button in benefits section and responsive styles

--- bright-space-client/src/pages/Home/Benefits/Benefits.jsx
+++ bright-space-client/src/pages/Home/Benefits/Benefits.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import BenefitCard from "../../../components/BenefitCard/BenefitCard";
 
@@ -38,10 +38,13 @@ const benefitInfos = [
 const Benefits = () => {
     return (
         <Box w='full' maxW='1596px' mx='auto' px={{ base: '2', lg: 8, '2xl': 0 }} mt={{ base: 10, md: 20, '2xl': 28 }}>
-            <SectionTitle
-            title='Benefits'
-            description='Discover the unique advantages of learning with Bright Space—flexible scheduling, expert instructors, and a diverse range of courses tailored to help you succeed in today’s fast-paced world. Empower your learning journey with tools and resources that fit your needs.'
-            />
+            <Box display='flex' flexDir={{ base: 'column', lg: 'row' }} alignItems={{ base: 'left', lg: 'center' }} justifyContent='space-between' gap='6'>
+                <SectionTitle
+                    title='Benefits'
+                    description='Discover the unique advantages of learning with Bright Space—flexible scheduling, expert instructors, and a diverse range of courses tailored to help you succeed in today’s fast-paced world. Empower your learning journey with tools and resources that fit your needs.'
+                />
+                <Button colorScheme='gray' w='max-content' size={{ base: 'md', md: 'lg' }}>View All</Button>
+            </Box>
             <Box className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-7' mt='16'>
                 {
                     benefitInfos.map((info, idx) => <BenefitCard key={idx} info={info} />)

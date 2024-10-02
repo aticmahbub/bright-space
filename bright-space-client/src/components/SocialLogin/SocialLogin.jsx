67759d0807f49cfedfa56e2c55450ddfb1324880
saidulislam28 <saidulislam79028@gmail.com>
2024-10-03 00:15:33 +0600

    fix some issues in login and signup page

--- bright-space-client/src/components/SocialLogin/SocialLogin.jsx
+++ bright-space-client/src/components/SocialLogin/SocialLogin.jsx
@@ -1,4 +1,4 @@
-const SocialLogin = ({googleLogin, githubLogin}) => {
+const SocialLogin = ({googleLogin}) => {
     return (
         <div className='space-y-5'>
              <button onClick={googleLogin}
@@ -12,17 +12,7 @@ const SocialLogin = ({googleLogin, githubLogin}) => {
                 />
                 <span>Sign Up with Google</span>
               </button>
-              <button onClick={githubLogin}
-                type="button"
-                className="w-full bg-[#F7F7F8] text-[#000000] border border-[#bab8b8] font-medium py-2 rounded-lg flex items-center justify-center space-x-2 hover:bg-gray-50"
-              >
-                <img
-                  src="https://cdn0.iconfinder.com/data/icons/free-social-media-set/24/github-512.png"
-                  alt="Google"
-                  className="w-5 h-5"
-                />
-                <span>Sign Up with Github</span>
-              </button>
+             
         </div>
     );
 };

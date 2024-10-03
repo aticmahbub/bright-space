1a51e1021d06287001a30801e516a50b0d18f736
jalal-uddin-sharif <hycute.ts@gmail.com>
2024-10-04 01:37:22 +0600

    modified course content design

--- bright-space-client/src/components/CourseContent/CourseContent.jsx
+++ bright-space-client/src/components/CourseContent/CourseContent.jsx
@@ -1,60 +1,27 @@
 import { Box, Icon, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
-import {
-  MdKeyboardArrowDown,
-  MdOutlineKeyboardArrowRight,
-} from "react-icons/md";
-import { RxVideo } from "react-icons/rx";
-import { CiLock } from "react-icons/ci";
+
+import CourseContentType from "./CourseContentType";
 
 const CourseContent = () => {
   const [advancedContent, setAdvancedContent] = useState(false);
+  const [basicContent, setBasicContent] = useState(false);
   return (
     <Box mt={"70px"}>
       <Text fontWeight={"medium"} fontSize={"lg"} my={"7"}>
         Course content
       </Text>
-      <Box border={"0.5px solid #376bf1"}  borderRadius={"lg"} overflow={"hidden"} bg={"white"}>
-        <Box
-          display="flex"
-          alignItems="center"
-          justifyContent={"space-between"}
-          borderBottom={advancedContent ? "1px solid orange" : "none"}
-          p={"4"}
-          cursor={"pointer"}
-          userSelect={"none"}
-          transition="all 0.3s ease-in" 
-          onClick={() => setAdvancedContent(!advancedContent)}
-        >
-          <Text fontSize={"xl"}>Advanced Concepts</Text>
-          <Icon
-            fontSize={"2xl"}
-            as={
-              advancedContent
-                ? MdKeyboardArrowDown
-                : MdOutlineKeyboardArrowRight
-            }
-          ></Icon>
-        </Box>
-
-        {/* {advancedContent && ( */}
-          <>
-            <Box className="space-y-4" display={ advancedContent ? "" : "none"} >
-
-              <Box _hover={{bg:"gray.100"}} display={"flex"} justifyContent={"space-between"} px={"4"} py={"2"}>
-                <Box display={"flex"} alignItems={"center"} gap={"4"}>
-                  <Icon fontSize={"xl"} as={RxVideo}></Icon>
-                  <Text fontSize={"xl"}>Meal Planning Explained</Text>
-                </Box>
-                <Box display={"flex"} alignItems={"center"} gap={"4"}>
-                  <Text fontSize={"lg"}>07:00</Text>
-                  <Icon fontSize={"xl"} as={CiLock}></Icon>
-                </Box>
-              </Box>
-
-            </Box>
-          </>
-        {/* )} */}
+      <Box className="space-y-6">
+        <CourseContentType
+          advancedContent={advancedContent}
+          setAdvancedContent={setAdvancedContent}
+          title={"Advanced Concepts"}
+        />
+        <CourseContentType
+          advancedContent={basicContent}
+          setAdvancedContent={setBasicContent}
+          title={"Basic Contents"}
+        />
       </Box>
     </Box>
   );

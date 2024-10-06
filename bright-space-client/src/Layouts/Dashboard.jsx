5c60c0ac14d8dd3fef4bfdf3ff038336c82457f0
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-10-06 16:43:52 +0600

    Complete

--- bright-space-client/src/Layouts/Dashboard.jsx
+++ bright-space-client/src/Layouts/Dashboard.jsx
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { useState } from "react";
 import brightSpace_logo from "../assets/bright-space-logo.svg";
 import { FcMenu } from "react-icons/fc";
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { FaUser } from "react-icons/fa";
+import { PiChalkboardTeacherFill } from "react-icons/pi";
+import { BiSolidBookContent } from "react-icons/bi";
+import { IoMdSettings } from "react-icons/io";
+
+
+
 
 
 const Dashboard = () => {
@@ -9,16 +17,44 @@ const Dashboard = () => {
 
     const toggleSidebar = () => setIsSidebarOpen(prev => !prev);
 
+    const SideNavList = [
+        {
+            name: 'Dashboard',
+            path: '/dashBoard',
+            icon: TbLayoutDashboardFilled
+        },
+        {
+            name: 'All Students',
+            path: 'allStudents',
+            icon: FaUser
+        },
+        {
+            name: 'All Teachers',
+            path: 'allStudents',
+            icon: PiChalkboardTeacherFill
+        },
+        {
+            name: 'Courses',
+            path: 'allStudents',
+            icon: BiSolidBookContent 
+        },
+        {
+            name: 'Setting',
+            path: 'allStudents',
+            icon: IoMdSettings 
+        }
+    ]
+
     return (
         <div className="flex">
             {/* Sidebar */}
             <div className={`fixed min-h-screen bg-white shadow-sm shadow-primary-200 z-20 hidden lg:block`}>
-                <div className={`transition-all duration-300  h-screen overflow-hidden group ${isSidebarOpen ? 'w-72' : 'w-16 hover:w-72'}`}>
+                <div className={`transition-all duration-300 p-4 h-screen group ${isSidebarOpen ? 'w-72' : 'w-16 hover:w-72'}`}>
                     {/* Logo And Text */}
-                    <div className="p-4 whitespace-nowrap flex items-center w-full justify-between">
+                    <div className="flex pl-2 items-center w-full justify-between">
                         <div className="flex items-center space-x-3">
-                            <img className="w-6" src={brightSpace_logo} alt="Bright Space Logo"/>
-                            <h1 className={`text-lg font-bold transition-opacity duration-300 text-primary-500 ${isSidebarOpen ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}>
+                            <img className="w-6" src={brightSpace_logo} alt="Bright Space Logo" />
+                            <h1 className={`text-lg font-bold transition-opacity duration-300 text-primary-500 ${isSidebarOpen ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'} overflow-hidden whitespace-nowrap `}>
                                 Bright Space
                             </h1>
                         </div>
@@ -26,11 +62,25 @@ const Dashboard = () => {
                             className={`text-3xl p-2 rounded-full transition-all duration-500 hover:bg-primary-100 ${isSidebarOpen ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'}`}
                             onClick={toggleSidebar}
                         >
-                            <FcMenu />
+                            <FcMenu className="text-[#8094AE]"/>
                         </button>
                     </div>
                     {/* List Item */}
-                    
+                    <nav className="mt-10">
+                        <ul className=" space-y-2">
+                            {
+                                SideNavList.map((item, idx) =>
+                                    <li key={idx}>
+                                        <NavLink to={item.path} className={'transition-all duration-300 text-lg grid grid-cols-[auto_1fr] items-center px-2 py-3 text-[#8094AE] space-x-3 rounded-lg hover:bg-[#EBEEF2] hover:text-primary-500'}>
+                                            <item.icon className="inline-block text-[24px]" />
+                                            <p className={`${isSidebarOpen ? 'opacity-100' : 'opacity-0 group-hover:opacity-100'} transition-opacity duration-500 inline-block font-semibold text-base overflow-hidden whitespace-nowrap`}>{item.name}</p>
+                                        </NavLink>
+                                    </li>
+                                )
+                            }
+                        </ul>
+                    </nav>
+
                 </div>
             </div>
             {/* Main Content */}
@@ -40,7 +90,7 @@ const Dashboard = () => {
                     <h1 className="text-xl font-semibold text-left bg-white">Dashboard</h1>
                 </div>
                 {/* Outlet for Main Content */}
-                <div className={`pt-16 h-full transition-all duration-700 bg-about_banner_background_color ${isSidebarOpen ? 'pl-80' : 'lg:pl-24'}`}>
+                <div className={`pt-16 h-full transition-all duration-700 bg-[#F5F6FA] ${isSidebarOpen ? 'pl-80' : 'lg:pl-24'}`}>
                     <Outlet />
                 </div>
             </div>

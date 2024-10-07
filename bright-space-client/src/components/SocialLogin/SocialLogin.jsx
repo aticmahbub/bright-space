c425b79252352b04783d329aa4d9cec9d8357b56
Abu Naim <abunaim140@gmail.com>
2024-10-07 19:49:53 +0600

    modify the registeration page and added some changes in login and sociallogin component

--- bright-space-client/src/components/SocialLogin/SocialLogin.jsx
+++ bright-space-client/src/components/SocialLogin/SocialLogin.jsx
@@ -1,20 +1,12 @@
-const SocialLogin = ({googleLogin}) => {
-    return (
-        <div className='space-y-5'>
-             <button onClick={googleLogin}
-                type="button"
-                className="w-full bg-[#F7F7F8] text-[#000000] border border-[#bab8b8] font-medium py-2 rounded-lg flex items-center justify-center space-x-2 hover:bg-gray-50"
-              >
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/480px-Google_%22G%22_logo.svg.png"
-                  alt="Google"
-                  className="w-5 h-5"
-                />
-                <span>Sign Up with Google</span>
-              </button>
-             
-        </div>
-    );
+import { Button, Icon } from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
+const SocialLogin = ({ googleLogin }) => {
+  return (
+    <Button onClick={googleLogin} colorScheme='gray' w='full' gap='2'>
+      <Icon fontSize='2xl' as={FcGoogle} />
+      <span>Sign Up with Google</span>
+    </Button>
+  );
 };
 
 export default SocialLogin;
--- bright-space-client/src/pages/shared/Authentication/Registration/Registration.jsx
+++ bright-space-client/src/pages/shared/Authentication/Registration/Registration.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import SocialLogin from "../../../../components/SocialLogin/SocialLogin";
 import useAuth from "../../../../hooks/useAuth";
 import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import { Box, Button } from "@chakra-ui/react";
+import Lottie from "lottie-react";
+import authentication from '../../../../Lotties/authentication.json'
 
 const Registration = () => {
-  const { createUser, googleLogin, updateUserProfile } = useAuth();
+  const { createUser, updateUserProfile } = useAuth();
   const axiosPublic = useAxiosPublic();
   const navigate = useNavigate();
-  
+
   const [formData, setFormData] = useState({
     fullName: "",
     email: "",
@@ -21,12 +23,12 @@ const Registration = () => {
   const [error, setError] = useState(""); // Error state
 
   const handleChange = (e) => {
-    const { name, value, type, checked,} = e.target;
+    const { name, value, type, checked, } = e.target;
     setFormData({
       ...formData,
       [name]: type === "checkbox" ? checked : value,
     });
-    
+
     // Clear error when user starts typing
     if (error) setError("");
   };
@@ -43,17 +45,17 @@ const Registration = () => {
           .then(() => {
             console.log('user profile updated');
 
-            const userInfo={
+            const userInfo = {
               name: fullName,
               email: email,
               role: formData?.role,
             }
-            axiosPublic.post('/users',userInfo)
-            .then(res =>{
-              if(res.data.insertedI){
-                console.log('user added to database');
-              }
-            })
+            axiosPublic.post('/users', userInfo)
+              .then(res => {
+                if (res.data.insertedI) {
+                  console.log('user added to database');
+                }
+              })
             navigate('/')
           })
           .catch(error => {
@@ -64,143 +66,128 @@ const Registration = () => {
   };
 
   return (
-   
-      <div className="flex flex-col md:flex-row items-center justify-center gap-5 lg:gap-10 max-w-[1440px] mx-auto">
-        <div>
-          <dotlottie-player
-            src="https://lottie.host/d04a48fe-d3f4-4ce1-ba2a-9b6b239d3ee0/cLJoUwPZPC.json"
-            background="transparent"
-            speed="1"
-            loop
-            autoplay
-          ></dotlottie-player>
-        </div>
+    <Box maxW='1596px' mx='auto' px={{ base: '2', lg: '60px', '2xl': 0 }} my={{ base: 0, lg: 20, xl: 0 }} mb={{ base: 20, lg: 0 }}>
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-5 lg:gap-10">
+        <Lottie className='w-full flex-1' animationData={authentication} />
+        <div className="p-7 md:p-10 2xl:p-12 bg-white lg:max-w-[460px] 2xl:max-w-[560px] rounded-lg flex-1">
+          <h2 className="text-2xl font-bold text-center mb-4">Sign Up</h2>
+          <p className="text-center text-gray-500 mb-6">
+            Create an account to unlock exclusive features.
+          </p>
 
-        <div className="flex justify-center items-center w-full p-2 md:p-10">
-          <div className="p-8 rounded-lg shadow-lg w-full bg-[#ffff]">
-            <h2 className="text-2xl font-bold text-center mb-4">Sign Up</h2>
-            <p className="text-center text-gray-500 mb-6">
-              Create an account to unlock exclusive features.
-            </p>
+          <form onSubmit={handleSubmit} className="space-y-4">
 
-            <form onSubmit={handleSubmit} className="space-y-4">
-              
 
-              <div className="text-start flex flex-col">
-                <label className="mb-2 font-medium text-gray-700">Full Name</label>
-                <input
-                  type="text"
-                  name="fullName"
-                  value={formData.fullName}
-                  onChange={handleChange}
-                  placeholder="Enter your Name"
-                  className="w-full px-4 py-3 border border-gray-100 rounded-lg bg-[#FCFCFD] focus:outline-none focus:ring focus:ring-indigo-200"
-                  required
-                />
+            <div className="text-start flex flex-col">
+              <label className="mb-2 font-medium text-gray-700">Full Name</label>
+              <input
+                type="text"
+                name="fullName"
+                value={formData.fullName}
+                onChange={handleChange}
+                placeholder="Enter your Name"
+                className="w-full px-4 py-3 border border-gray-100 rounded-lg bg-[#FCFCFD] focus:outline-none focus:ring focus:ring-indigo-200"
+                required
+              />
+            </div>
+
+            <div className="text-start flex flex-col">
+              <label className="mb-2 font-medium text-gray-700">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter your Email"
+                className="w-full px-4 py-3 border border-gray-100 bg-[#FCFCFD] rounded-lg focus:outline-none focus:ring focus:ring-indigo-200"
+                required
+              />
+            </div>
+
+            <div className="text-start flex flex-col">
+              <label className="mb-2 font-medium text-gray-700">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Enter your Password"
+                className="w-full px-4 py-3 border border-gray-100 bg-[#FCFCFD] rounded-lg focus:outline-none focus:ring focus:ring-indigo-200"
+                required
+              />
+            </div>
+
+            <div className="text-start flex flex-col">
+              <label className="mb-2 font-medium text-gray-700">Confirm Password</label>
+              <input
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                placeholder="Confirm your Password"
+                className="w-full px-4 py-3 border border-gray-100 bg-[#FCFCFD] rounded-lg focus:outline-none focus:ring focus:ring-indigo-200"
+                required
+              />
+            </div>
+
+            {error && (
+              <div className=" text-[#ff0a0a] ">
+                {error}
               </div>
-              
-              <div className="text-start flex flex-col">
-                <label className="mb-2 font-medium text-gray-700">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  placeholder="Enter your Email"
-                  className="w-full px-4 py-3 border border-gray-100 bg-[#FCFCFD] rounded-lg focus:outline-none focus:ring focus:ring-indigo-200"
-                  required
-                />
-              </div>
-              
-              <div className="text-start flex flex-col">
-                <label className="mb-2 font-medium text-gray-700">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  placeholder="Enter your Password"
-                  className="w-full px-4 py-3 border border-gray-100 bg-[#FCFCFD] rounded-lg focus:outline-none focus:ring focus:ring-indigo-200"
-                  required
-                />
-              </div>
+            )}
 
-              <div className="text-start flex flex-col">
-                <label className="mb-2 font-medium text-gray-700">Confirm Password</label>
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  value={formData.confirmPassword}
-                  onChange={handleChange}
-                  placeholder="Confirm your Password"
-                  className="w-full px-4 py-3 border border-gray-100 bg-[#FCFCFD] rounded-lg focus:outline-none focus:ring focus:ring-indigo-200"
-                  required
-                />
-              </div>
-
-              {error && (
-                <div className=" text-[#ff0a0a] ">
-                  {error}
-                </div>
-              )}
-
-              <div className="inline-block py-2 px-4 bg-[#FCFCFD] rounded-lg font-medium">
-                Role:
-                <select
-                  className="border ml-2 rounded-lg font-normal"
-                  name="role"
-                  value={formData.role}
-                  onChange={handleChange} // Handle change for role
-                >
-                  <option value="student">Student</option>
-                  <option value="teacher">Teacher</option>
-                </select>
-              </div>
-
-              <div className="flex items-center">
-                <input
-                  type="checkbox"
-                  name="termsAccepted"
-                  checked={formData.termsAccepted}
-                  onChange={handleChange}
-                  className="h-4 w-4 text-orange-500 border-gray-300 rounded"
-                />
-                <label className="ml-2 text-sm text-gray-600">
-                  I agree with{" "}
-                  <a href="#" className="text-orange-500 hover:underline">
-                    Terms of Use {" "}
-                  </a>
-                  and
-                  <a href="#" className="text-orange-500 hover:underline">
-                   {" "} Privacy Policy
-                  </a>
-                </label>
-              </div>
-
-              <button
-                type="submit"
-                className="w-full py-2 rounded-lg text-[#ffff] 
-                  formData.termsAccepted  bg-[#FF9500] hover:bg-[#f3b051]" 
-                
+            <div className="inline-block py-2 px-4 bg-[#FCFCFD] rounded-lg font-medium">
+              Role:
+              <select
+                className="border ml-2 rounded-lg font-normal"
+                name="role"
+                value={formData.role}
+                onChange={handleChange} // Handle change for role
               >
-                Sign Up
-              </button>
+                <option value="student">Student</option>
+                <option value="teacher">Teacher</option>
+              </select>
+            </div>
 
-              <div className="text-center text-gray-500">OR</div>
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                name="termsAccepted"
+                checked={formData.termsAccepted}
+                onChange={handleChange}
+                className="h-4 w-4 text-orange-500 border-gray-300 rounded"
+              />
+              <label className="ml-2 text-sm text-gray-600">
+                I agree with{" "}
+                <a href="#" className="text-orange-500 hover:underline">
+                  Terms of Use {" "}
+                </a>
+                and
+                <a href="#" className="text-orange-500 hover:underline">
+                  {" "} Privacy Policy
+                </a>
+              </label>
+            </div>
 
-              <SocialLogin googleLogin={googleLogin} />
+            <Button
+              w='full'
+              type="submit"
+              colorScheme='primary'
+            >
+              Sign Up
+            </Button>
 
-              <p className="text-center text-gray-500 mt-4">
-                Already have an account?{" "}
-                <Link to={"/login"} className="text-orange-500 hover:underline">
-                  Sign In
-                </Link>
-              </p>
-            </form>
-          </div>
+            <p className="text-center text-gray-500 mt-4">
+              Already have an account?{" "}
+              <Link to={"/login"} className="text-orange-500 hover:underline">
+                Sign In
+              </Link>
+            </p>
+          </form>
         </div>
       </div>
-    
+    </Box >
+
   );
 };
 

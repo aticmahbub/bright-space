31905273a52609c40ca647d5168e263e3c82ea1b
Asaduzzaman70 <asdudjamanhisam@gmail.com>
2024-10-10 04:56:01 +0600

    Added messages section

--- bright-space-client/src/components/Messages/Messages.jsx
+++ bright-space-client/src/components/Messages/Messages.jsx
@@ -1,46 +1,39 @@
 import {
+    Icon,
     Menu,
     MenuButton,
     MenuList,
-    MenuItem,
-    Icon,
-    Flex,
-    Image,
-    Box,
-    Text,
 } from '@chakra-ui/react'
-import { FaCheck, FaEnvelope } from "react-icons/fa";
+import { useEffect, useState } from 'react';
+import { FaEnvelope } from "react-icons/fa";
+import MessageItem from './MessageItem/MessageItem';
 
 
 const Messages = () => {
+    const [messages, setMessages] = useState([]);
 
-    
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/message.json');
+            const data = await response.json();
+            setMessages(data);
+        };
+
+        fetchData();
+    }, []);
+
+    console.log(messages);
+
     return (
         <div>
             <Menu>
                 <MenuButton>
-                    <Icon as={FaEnvelope} />
+                    <Icon as={FaEnvelope}  color={'gray.500'}/>
                 </MenuButton>
-                <MenuList>
-                    <MenuItem>
-                        <Flex>
-                            <Image src="https://via.placeholder.com/150" borderRadius="full" boxSize="40px" />
-                            <Box ml="3">
-                                <Text fontWeight="bold">
-                                    John Doe
-                                </Text>
-                                <Text fontSize="sm">
-                                    This is a test message
-                                </Text>
-                            </Box>
-                            <Box ml="auto">
-                                <Text fontSize="sm" color="gray.500">
-                                    12:00 PM
-                                </Text>
-                                <Icon as={FaCheck} color="gray.500" />
-                            </Box>
-                        </Flex>
-                    </MenuItem>
+                <MenuList overflowY="auto" maxH="300px" mt={'10px'}>
+                    {
+                        messages.map(message => <MessageItem key={message.id} message={message} />)
+                    }
                 </MenuList>
             </Menu>
         </div>

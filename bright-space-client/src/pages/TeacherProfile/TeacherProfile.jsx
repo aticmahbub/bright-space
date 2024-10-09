fa255d74dc24da44a95a2bbcc9521d8685ccb129
jalal-uddin-sharif <hycute.ts@gmail.com>
2024-10-09 11:38:43 +0600

    designed profiling system

--- bright-space-client/src/pages/TeacherProfile/TeacherProfile.jsx
+++ bright-space-client/src/pages/TeacherProfile/TeacherProfile.jsx
@@ -1,21 +1,34 @@
-import { Box, Icon, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Icon, Image, Stack, Text } from "@chakra-ui/react";
 import { BsThreeDots } from "react-icons/bs";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoMdCall } from "react-icons/io";
 import { MdEmail } from "react-icons/md";
+import PersonalDetails from "../../components/StudentTeacherProfile/PersonalDetails";
+import { useState } from "react";
 
 const TeacherProfile = () => {
+  const [option, SetOption] = useState(false)
   return (
-    <Box border={"1px solid orange"} p={4} rounded={"2xl"}>
-      <Box border={"1px solid orange"} rounded={"2xl"} overflow={"hidden"}>
+    <Box border={"1px solid"} borderColor={"primary.100"} p={4} rounded={"2xl"}>
+      <Box
+        border={"1px solid"}
+        borderColor={"primary.100"}
+        rounded={"2xl"}
+        overflow={"hidden"}
+      >
         <Image
           src="https://i.imghippo.com/files/r7bNH1728411035.jpg"
           h={"32"}
           w={"full"}
         />
-        <Box p={4}>
+        <Box>
           {/* Profile overview */}
-          <Box display={"flex"} justifyContent={"space-between"}>
+          <Box
+            display={"flex"}
+            justifyContent={"space-between"}
+            p={4}
+            borderBottom={"1px solid orange"}
+          >
             <Box>
               <Image
                 src="https://i.imghippo.com/files/M10af1728411363.jpg"
@@ -23,7 +36,7 @@ const TeacherProfile = () => {
                 w={"36"}
                 rounded={"full"}
               />
-              <Text w={"700px"}>
+              <Text w={"700px"} my={4}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
                 totam blanditiis dolores nemo ducimus fugiat magnam laboriosam
                 labore expedita corporis.
@@ -74,8 +87,26 @@ const TeacherProfile = () => {
               </Box>
             </Box>
             <Box>
-              <BsThreeDots size={30} cursor={"pointer"} />
+              <Box display="flex" justifyContent="end" userSelect="none">
+                <BsThreeDots size={30} cursor={"pointer"} onClick={()=> SetOption(true)} />
+              </Box>
+              {
+                option && 
+                <Box bg={"primary.100"} p="4" rounded={"xl"}>
+                <Stack>
+                  <Text>All Exam Result</Text>
+                  <Text>Attendence</Text>
+                  <Button size="xs" colorScheme="orange" w="full" onClick={()=> SetOption(false)}>
+                    Close
+                  </Button>
+                </Stack>
+              </Box>
+              }
+              
             </Box>
+          </Box>
+          <Box px={4}>
+            <PersonalDetails />
           </Box>
         </Box>
       </Box>

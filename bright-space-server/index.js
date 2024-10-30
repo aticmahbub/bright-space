6e70a731ef057bbea4e77cf09cd4e28c8f6f6ecb
aticcmahbub <aticcmahbub@gmail.com>
2024-10-30 15:47:47 +0600

    update

--- bright-space-server/index.js
+++ bright-space-server/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://bright-space.netlify.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   },
 });
